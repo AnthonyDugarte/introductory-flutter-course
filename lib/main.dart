5ec8cfbbfd2dac9dc33d2fd1647278efac34fa0b
Anthony Dugarte <toonny1998@gmail.com>
2019-10-02 09:14:06 -0400

    Split code

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,4 @@
 import 'package:flutter/material.dart';
+import 'views/home.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  int n = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Example App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text("My Example App"),
-          ),
-          leading: Icon(Icons.menu),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text("Clicks: $n"),
-            RaisedButton(
-              child: Text("Acc"),
-              onPressed: addCounter,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void addCounter() {
-    setState(() => n++);
-  }
-}
